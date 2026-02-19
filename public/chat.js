97affbad2b0f345a8a645981bf570ac64991e962
buildwithzratham <buildwithzratham@gmail.com>
2026-02-19 22:22:19 +0530

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,20 @@
+async function sendMessage() {
+  const message = document.getElementById("message").value;
+  const token = localStorage.getItem("token");
+
+  const res = await fetch("https://studyforge-backend-bjlh.onrender.com/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+    body: JSON.stringify({ message })
+  });
+
+  const data = await res.json();
+
+  document.getElementById("reply").innerText = data.reply;
+  document.getElementById("credits").innerText = 
+    "Credits left: " + data.credits;
+}
 
