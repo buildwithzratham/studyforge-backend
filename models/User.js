db9fe9749bf988cfd177dbd806eae1d7bb251d96
buildwithzratham <buildwithzratham@gmail.com>
2026-02-20 20:00:21 +0530

    User.js

--- models/User.js
+++ models/User.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  credits: { type: Number, default: 1000 },
+  credits: { type: Number, default: 100 },
   premium: { type: Boolean, default: false },
   conversations: [conversationSchema]
 });
