e83b0eed37156f69fb28a4d388030e5b04fa784d
buildwithzratham <buildwithzratham@gmail.com>
2026-02-20 20:09:57 +0530

    User.js

--- models/User.js
+++ models/User.js
@@ -1,29 +1,16 @@
 import mongoose from "mongoose";
 
-const messageSchema = new mongoose.Schema({
-  role: String,
-  content: String
-}, { _id: false });
-
-const conversationSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    default: "New Chat"
-  },
-  messages: [messageSchema],
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-});
-
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   credits: { type: Number, default: 100 },
-  premium: { type: Boolean, default: false },
-  conversations: [conversationSchema]
+  messages: [
+    {
+      role: String,
+      content: String
+    }
+  ]
 });
 
 export default mongoose.model("User", userSchema);
