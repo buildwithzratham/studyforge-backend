73cb82a963e8a71c04fdda8983f23a0290ed8a90
buildwithzratham <buildwithzratham@gmail.com>
2026-02-21 01:01:16 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,6 +5,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   credits: { type: Number, default: 100 },
+  totalMessages: { type: Number, default: 0 },
+totalTokensUsed: { type: Number, default: 0 },
   messages: [
     {
       role: String,
