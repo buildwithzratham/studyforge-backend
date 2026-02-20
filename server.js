2acc45d8c269f7fe3ec3ece42e4f77a753a5e731
buildwithzratham <buildwithzratham@gmail.com>
2026-02-20 15:43:24 +0530

    server.js

--- server.js
+++ server.js
@@ -154,8 +154,3 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
