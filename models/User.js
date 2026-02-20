775162c8b39376614194a86bd0c58080e5553d52
buildwithzratham <buildwithzratham@gmail.com>
2026-02-20 18:36:19 +0530

    User.js

--- models/User.js
+++ models/User.js
@@ -1,34 +1,17 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  credits: {
-    type: Number,
-    default: 10
-  },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  credits: { type: Number, default: 10 },
+  premium: { type: Boolean, default: false },
   messages: [
     {
       role: String,
       content: String
     }
-  ],
-  premium: {
-    type: Boolean,
-    default: false
-  }
+  ]
 });
 
 export default mongoose.model("User", userSchema);
